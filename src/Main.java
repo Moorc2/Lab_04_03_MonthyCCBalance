d76a60ce90a4d32e97be262d57363c1b43dbb2d7
Charles Moore <moorc2@mail.uc.edu>
2026-02-09 21:10:58 -0500

    finished program

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        double startingCCBalance = 5000;
+        double startingCCBalance = 5000.00;
         double interestRate = .17;
         double firstMonthBalance = startingCCBalance + (startingCCBalance * interestRate);
+        double secondMonthBalance = firstMonthBalance + (firstMonthBalance * interestRate);
 
-
-        System.out.println(firstMonthBalance);
-
-
+        System.out.println("Your initial card balance is $" + startingCCBalance);
+        System.out.println("The interest rate is: " + interestRate + "%");
+        System.out.println("Your balance after the first month will be: $" + firstMonthBalance);
+        System.out.println("Your balance after the second month will be: $" + secondMonthBalance);
 
     }
 }
