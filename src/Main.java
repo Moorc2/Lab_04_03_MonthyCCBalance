e70c257c5341937ead3d9679a61fe79b77259f7b
Charles Moore <moorc2@mail.uc.edu>
2026-02-09 21:06:48 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double startingCCBalance = 5000;
+        double interestRate = .17;
+        double firstMonthBalance = startingCCBalance + (startingCCBalance * interestRate);
+
+
+        System.out.println(firstMonthBalance);
+
+
+
+    }
+}
